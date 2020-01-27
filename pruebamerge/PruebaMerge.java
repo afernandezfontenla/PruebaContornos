890006de00f912d3f671a36c0c22d4ddc095e025
afernandezfontenla <56158212+afernandezfontenla@users.noreply.github.com>
2020-01-27 10:11:31 +0100

    Add files via upload

---
+++ pruebamerge/PruebaMerge.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebamerge;
+
+/**
+ *
+ * @author afernandezfontenla
+ */
+public class PruebaMerge {
+
+public static void main(String[] args) {
+
+    //sout(hello);
+    System.out.println("Hello");
+    
+}    
+}
